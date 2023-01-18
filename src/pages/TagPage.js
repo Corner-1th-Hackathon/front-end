dfd873841f2b0b5ac919a34c38b632ccee9b25a1
RJY <devdaradara@gamil.com>
2023-01-19 02:05:54 +0900

    tag 페이지

--- src/pages/TagPage.js
+++ src/pages/TagPage.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import './TagPage.css';
+import JanImage from '../images/month_jan.png';
+import WriteImage from '../images/planet.png';
+
 
 function TagPage() {
-  return <div>태그 페이지</div>;
+  return (
+    <div>
+      <MonthButton />
+      <div className="tagName">#태그</div>
+      <WriteButton />
+    </div>
+  );
+}
+
+// 메인페이지로 가는 버튼
+const MonthButton = () => {
+  return (
+    <NavLink to={'/'} className='monthButton'>
+      <img src={JanImage} alt='메인으로' />
+    </NavLink>
+  );
+};
+
+// 글쓰기 버튼
+const WriteButton = () => {
+  return (
+    <NavLink to={'/write'} className='wirteButton'>
+      <img src={WriteImage} alt='글작성'/>
+    </NavLink>
+  )
 }
 
 export default TagPage;
